c42a931886cab2588d620f6438abecb2a83414c9
Leschonander <33600597+Leschonander@users.noreply.github.com>
2018-07-29 21:10:35 -0400

    Charts
    I added a Bokeh chart to display some of the data to the project...

--- JumpBikeDataProject/app.py
+++ JumpBikeDataProject/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from data import bikes, chargeCount, chargeDesc
 import pandas as pd
 from features import bikeLocations #the features data...
+from bokeh.plotting import figure
+from bokeh.embed import components
 #Insert data here
 title='JumpBike Map and Viz'
 
@@ -24,6 +26,16 @@ def home():
 
 @app.route('/map')
 def mapboxBike():
+    #chart
+    charges = ["Low Level", "Medium Level", "High Level"]
+    plot = figure(plot_width=400, plot_height=400,
+        title="Bar Chart of Charge Level of Open Jump Bikes",
+        x_range=charges, tools="hover")
+    plot.vbar(x=charges, width = 0.5, bottom = 0,
+        top=[chargeCount["Low Charge"],chargeCount["Medium Charge"] ,chargeCount["High Charge"]],
+        color="crimson")
+    #chart
+    script, div = components(plot)
     return render_template(
         'map.html',
         title = title,
@@ -31,7 +43,8 @@ def mapboxBike():
         bikeLocations = bikeLocations,
         tables = [bikes.to_html(classes='dataset1')],
         chargeTable = [chargeCount.to_html(classes='dataset2')],
-        CdescribeTable = [chargeDesc.to_html(classes='dataset3')]
+        CdescribeTable = [chargeDesc.to_html(classes='dataset3')],
+        the_div=div, the_script=script
     )
 
 if __name__ == '__main__':
